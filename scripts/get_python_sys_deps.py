3a22fc4fba5b2f5c87e7ce4f7894624b82b6dad6
Toby Ho <toby@repl.it>
2024-01-31 18:25:34 +0000

    added scripts for building/using registry

---
+++ scripts/get_python_sys_deps.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# This script finds each referenced Nix package in UPM's python_map.json and adds them to replit.nix
+# It is meant to be used in a Repl
+
+import json
+import subprocess
+
+python_map_url = 'https://raw.githubusercontent.com/replit/upm/main/internal/nix/python_map.json'
+
+output = subprocess.check_output(['curl', python_map_url])
+data = json.loads(output)
+
+deps = set()
+
+deps.add('pkgs.xorg.libX11')
+deps.add('pkgs.xorg.libXcursor')
+deps.add('pkgs.xorg.libXinerama')
+deps.add('pkgs.xorg.libXext')
+deps.add('pkgs.xorg.libXrandr')
+deps.add('pkgs.xorg.libXi')
+deps.add('pkgs.xorg.libXxf86vm')
+
+deps.add('pkgs.stdenv.cc.cc.lib')
+deps.add('pkgs.zlib')
+
+for key, value in data.items():
+  for dep in value["deps"]:
+    deps.add(dep)
+
+
+output = """
+{pkgs}: {
+  deps = [
+    """ + "\n    ".join(deps) + """
+  ];
+}"""
+
+with open("replit.nix", "w") as f:
+  f.write(output)
+  print('Wrote replit.nix')
