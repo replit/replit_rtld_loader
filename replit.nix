823ef3b32a39a95aa1a8fa2260c3deae0f12e3ca
Toby Ho <toby@repl.it>
2024-01-31 17:36:06 +0000

    updated files

--- replit.nix
+++ replit.nix
@@ -1,15 +1,7 @@
 
 {pkgs}: {
   deps = [
-    pkgs.tk
-    pkgs.tcl
-    pkgs.qhull
-    pkgs.pkg-config
-    pkgs.gtk3
-    pkgs.gobject-introspection
-    pkgs.ghostscript
-    pkgs.freetype
-    pkgs.ffmpeg-full
-    pkgs.cairo
+    pkgs.gdb
+    pkgs.python310Full
   ];
 }
