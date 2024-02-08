32fda26760c7e6e9b81cd21987d058777ea04b87
Toby Ho <toby@repl.it>
2024-02-08 12:12:19 -0500

    added overlay

--- flake.nix
+++ flake.nix
@@ -6,23 +6,27 @@
     let pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
+    package = pkgs.stdenv.mkDerivation {
+      pname = "replit_rtld_loader";
+      version = "1";
+      src = ./.;
+      buildInputs = [pkgs.python310];
+      installPhase = ''
+        mkdir $out
+        mv rtld_loader.so $out/
+      '';
+    };
     in
     {
-      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        pname = "replit_rtld_loader";
-        version = "1";
-        src = ./.;
-        buildInputs = [pkgs.python310];
-        installPhase = ''
-          mkdir $out
-          mv rtld_loader.so $out/
-        '';
-      };
+      packages.x86_64-linux.default = package;
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           python310
           gnumake
         ];
+      };
+      overlays.default = final: prev: {
+        replit-rtld-loader = package;
       };
     };
 }
